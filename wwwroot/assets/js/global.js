a40dcc4d8c3af243f2ac936c13a9c9825b38a027
Lee Ryan <squashedfrog26@me.com>
2012-05-15 22:06:32 +0100

    images now being pulled in via Flickr feed
    Ajax request implemented to get first 20 images from a photo group.
    Response formatted in jsonp.

---
+++ wwwroot/assets/js/global.js
@@ -0,0 +1,46 @@
+/*
+ * POODLE - Flickr Gallery Global JS
+ * @version 1.0
+ * @author Team Poodle
+ * @requires jQuery Core 1.7.2 - http://www.jquery.com/
+*/
+
+var FLICKR = window.FLICKR || {};
+
+FLICKR.getImages = (function(){
+
+	var photoSetURL = 'http://api.flickr.com/services/feeds/groups_pool.gne?id=83376903@N00&format=json&jsoncallback=?';
+	
+	var createImgDomNode = function(imageUrl){
+		var imageNode = '<img src="' + imageUrl + '"/>';
+		$('body').append(imageNode);
+	}
+
+	var requestPhotos = function(){
+		$.ajax({
+			url: photoSetURL,
+			dataType: 'jsonp',
+			success: function(data){
+
+				var photoArr = data.items; 
+
+				for (i in photoArr){
+					createImgDomNode(data.items[i].media.m);
+				}
+
+
+			}
+		});
+	};
+
+	return{
+		init: function(){
+			requestPhotos();
+		} 
+	}
+
+}());
+
+$(document).ready(function(){
+	FLICKR.getImages.init();
+});
