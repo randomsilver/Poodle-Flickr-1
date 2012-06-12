fffc6c9c3c4560e6a027ceadfc9ef8768a990d85
Andrey Buligin <andrey.buligin@lbi.com>
2012-06-12 15:38:08 +0100

    Added flickr images fetching through Flickr API

--- wwwroot/assets/js/global.js
+++ wwwroot/assets/js/global.js
@@ -9,24 +9,38 @@ var FLICKR = window.FLICKR || {};
 
 FLICKR.getImages = (function(){
 
-	var photoSetURL = 'http://api.flickr.com/services/feeds/groups_pool.gne?id=83376903@N00&format=json&jsoncallback=?';
-	
 	var createImgDomNode = function(imageUrl){
 		var imageNode = '<img src="' + imageUrl + '" id="image'+ i +'"/>';
 		$('#image-gallery').append(imageNode, i);
 	};
 
 	var requestPhotos = function(){
+		var AUTH_TOKEN = '72157630111491400-e73df37bd10f1fc8',
+			API_KEY = 'efa327fa27e64f3a0692c3ffb7f8ed5c',
+			API_SIG = '0f8a80506649f998ba312e8df70e1f6d',
+			GROUP_ID = '62496320%40N00',
+			photoSetURL,
+			showOnPage = 8,
+			photoString = '';
+        
+        var photoSetURL = 'http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key='
+        					+ API_KEY + '&group_id=' + GROUP_ID +'&per_page=' + showOnPage + '&format=json'
+        					+ '&nojsoncallback=1&auth_token=' + AUTH_TOKEN + '&api_sig=' + API_SIG;
+		
 		$.ajax({
 			url: photoSetURL,
-			dataType: 'jsonp',
+			dataType: 'json',
 			success: function(data){
 
-				var photoArr = data.items;
+				$.each(data.photos.photo, function(i, rPhoto){
+					var basePhotoURL = 'http://farm' + rPhoto.farm + '.static.flickr.com/' + rPhoto.server + '/' + rPhoto.id + '_' + rPhoto.secret,  
+						thumbPhotoURL = basePhotoURL + '_s.jpg',
+						mediumPhotoURL = basePhotoURL + '.jpg';
 
-				for (i in photoArr){
-					createImgDomNode(data.items[i].media.m, i);
-				}
+					photoString = photoString + '<a ' + 'title="' + rPhoto.title + '" href="'+ mediumPhotoURL +'"><img src="' + thumbPhotoURL + '" alt="' + rPhoto.title + '"/></a>';           
+				});
+				 
+				$(photoString).appendTo("#flickr-gallery");
 
 			}
 		});
@@ -34,7 +48,7 @@ FLICKR.getImages = (function(){
 
 	return{
 		init: function(){
-			//requestPhotos();
+			requestPhotos();
 			FLICKR.addFilter.init();
 		}
 	};
